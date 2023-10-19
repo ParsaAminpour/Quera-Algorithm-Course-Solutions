61ffb1a92769c9d177822cb44e72bbf89550c3f1
Parsa <parsa.aminpour@gmail.com>
2023-10-19 15:13:10 +0330

    solve problem NO.3

--- Season3/Solutions/SolutionNo3.py
+++ Season3/Solutions/SolutionNo3.py
@@ -1,11 +1,20 @@
 n, x = map(int, input().split(' '))
 coefficients = list(map(int, input().split(' ')))
-coefficients.reverse()
+# coefficients.reverse()
 
-result = 0
-for idx, a in enumerate(coefficients):
-    result += a * x**idx
+# answers = []
+# for i in range(n):
+#     result = x * (answers[i]) + coefficients[i]
+#     answers.append(result) #a0 , a1x, a2x^2 ,....
+    
+def horner_evaluation():
+    result = 0
+    for coefficient in coefficients[::-1]:
+        result = result * x + coefficient
+    return result
 
-
-print(result % (1e9 + 7))
+    
+if __name__ == '__main__':
+    answer = horner_evaluation()
+    print(answer % int(1e9 + 7))
 
