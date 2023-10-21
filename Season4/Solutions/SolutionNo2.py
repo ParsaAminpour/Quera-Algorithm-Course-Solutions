ef5b2bae76b47a57c8234378e9f20e3646061381
Parsa <parsa.aminpour@gmail.com>
2023-10-21 10:31:13 +0330

    recorrect SolutionNo2 of Season4

--- Season4/Solutions/SolutionNo2.py
+++ Season4/Solutions/SolutionNo2.py
@@ -4,6 +4,7 @@ sys.setrecursionlimit(10**6)
 a, b = map(int, input().split())
 
 
+# way No.1
 def f(m, n):
     if m == n: return m
     if m*n == 0: return 0
@@ -17,9 +18,32 @@ def f(m, n):
         if x == 0 or m % x == 0: return x
         else: return f(m, x) 
     
+
+# Way Nn.2 (Not appropriate for large numbers)
+def gcd(m,n):
+    if m == n: return m
+    if m*n == 0: return 0
+
+    if m > n:
+        tmp = m - n
+        if m % tmp == 0: return tmp
+        else : return gcd(n, tmp)
     
+    else:
+        tmp = n - m
+        if n % tmp == 0: return tmp
+        else: return gcd(m ,tmp)
+
+# Way No.3 (Appropriate for large numbers to calculate gcd)
+def gcd2(m, n):
+    while n:
+        m, n = n, m % n
+    return m
+
+
+
 if __name__ == '__main__':
-    result = f(a,b)
+    result = gcd2(a,b)
     print(result)
     
     
