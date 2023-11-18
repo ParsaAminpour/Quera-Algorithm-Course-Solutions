c3cdc7a02789ac216576c99f26875543018bbee1
Parsa <parsa.aminpour@gmail.com>
2023-11-18 14:24:04 +0330

    solutions belong season5 (greedy algorithm have been solved

--- Season5/Solutions/SolutionNo2.py
+++ Season5/Solutions/SolutionNo2.py
@@ -2,27 +2,42 @@ from rich import print
 n, k = map(int, input().split(' '))
 numbers = [int(num) for num in input().split()]
 
-partitions = []
-tmp = []
+# max_len = n - (k-1)
 
-for j in range(3):
-    tmp_partitions = []
-    tmp_partitions.append(numbers[:j])
+# results = []
+# tmp_list = []
+# for r in range(k):
+#     if r != 0 : results.append([numbers[r-1]])
 
-    for i in range(j, len(numbers)):
-        tmp.append(numbers[i])
-        if (i+1-j) % k == 0 or i == len(numbers)-1:
-            tmp_partitions.append(tmp)
-            tmp = []
+#     for i in range(r, len(numbers)):
+#         tmp_list.append(numbers[i])
+        
+#         if (i+1) % (max_len+r) == 0 or i == (len(numbers)-1): 
+#             results.append(tmp_list)
+#             tmp_list = []
 
-    partitions.append(tmp_partitions)
-    tmp_partitions = []
-
-print(partitions)
+# print(results)
 
 
 
-# sum_partitions = list(map(lambda x : sum(x), partitions))
-# print(sum_partitions)
+class Solution:
+    def __init__(self, n:int, k:int, numbers:list):
+        self.n = n
+        self.k = k
+        self.numbers = numbers
+        self.partition_count = 0
+        self.start_idx = 0
+        self.results = []
+        self.max_len = n - (k - 1)
 
-# print(min(sum_partitions))
+    # @param start_idx -> start_idx + 1
+    def judge(self, start_idx:int, partition_count:int) -> list:
+        rest_elemts_count = len(self.numbers) - start_idx
+        if rest_elemts_count < self.max_len: # it's mean we reach to the end of elements which partition will create in strict mode
+            rest_partition_len = k - self.partition_count
+
+            for i in range(rest_partition_len):
+                pass
+
+            for i in range(start_idx, len(self.numbers)):
+                self.results.append(self.numbers[i:rest_partition_len])
---
+++ Season5/Solutions/SolutionNo3.py
@@ -0,0 +1,23 @@
+import numpy as np
+# k -> init_energy
+n ,k = map(int, input().split(' '))
+
+friuts = []     #[[bi],[ai]] -> [- , +]
+for i in range(n):
+    data = list(map(int, input().split(' ')))
+    friuts.append(data)
+
+efficient_friuts_= np.array(list(filter(lambda x : x[0] < x[1], friuts)))
+sorted_efficient_friuts = efficient_friuts_[np.argsort(efficient_friuts_[:, 0])]
+
+results = []
+init_energy = k
+for f in sorted_efficient_friuts:
+    if init_energy - f[0] > 0:
+        results.append(list(f))
+        init_energy += abs(f[1]-f[0]) # abs is not necessary in here
+
+
+result_friuts = list(map(lambda x : abs(x[0] - x[1]), results))
+
+print(k + sum(result_friuts))
